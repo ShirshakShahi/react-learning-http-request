e75e3eef1593e36df1a6493c7678fe4c48b86950
ShirshakShahi <royalshirshal000@gmail.com>
2023-05-25 17:54:10 +0545

    used async-await here

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-## HTTP request in ReactJs.
+## using async await
+
+const getData = async () => {
+  try {
+    const response = await fetch('https://api.example.com/data');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+};
+
+getData()
+  .then(data => {
+    console.log('Data:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
--- src/App.js
+++ src/App.js
@@ -7,27 +7,23 @@ const App = () => {
 
   const [movies, setMovies] = useState([]);
 
-  const fetchMovieHandler = () => {
+  const fetchMovieHandler = async () => {
 
-    fetch('https://swapi.dev/api/films/').then(response => {
-      return response.json()
-    }).then(data => {
+    const response = await fetch('https://swapi.dev/api/films/');
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies);
-    }
-    );
+    const data = await response.json();
 
+    const transformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date
+      }
+    })
+
+    setMovies(transformedMovies);
   }
-
-
 
   return (
     <React.Fragment>
